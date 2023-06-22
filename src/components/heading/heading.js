585eefddac430f9a977318e5d9152b6521a97c44
Rohit Jha <me@rjha.dev>
2023-06-22 12:36:47 +0530

    feat: create next app

--- src/components/heading/heading.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./heading.module.scss";
-
-function Heading(props) {
-  return <div className={styles.heading}>{props.children}</div>;
-}
-export default Heading;
