2a157764193c71ff3aad449227bc128b4bd2fbeb
Rohit Jha <me@rjha.dev>
2023-10-02 17:59:27 +0530

    fix: font sizing for rem

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={styles.html}>
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
