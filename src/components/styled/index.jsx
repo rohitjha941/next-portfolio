702c85c307aad56dafc5e255c906f65bdae76bb7
Rohit Jha <me@rjha.dev>
2025-03-04 04:12:50 +0530

    css refactor

--- src/components/styled/index.jsx
+++ src/components/styled/index.jsx
@@ -5,13 +5,13 @@ export const Text = styled.div`
   font-size: 1.5rem;
   line-height: 1.875rem;
   letter-spacing: 0;
-  color: #a1a1a1;
+  color: var(--gray);
   opacity: 1;
 `;
 
 export const BlogsStructure = styled.div`
     margin: 0 18.75rem;
-    background: #2E2E2E 0% 0% no-repeat padding-box;
+    background: var(--background-color-2) 0% 0% no-repeat padding-box;
     opacity: 1;
     padding: 7.625rem 11.25rem;
 `;
