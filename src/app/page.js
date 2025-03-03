c0c18f927c23d20888375137ac0f813ca3b6becf
Rohit Jha <me@rjha.dev>
2025-03-04 01:01:17 +0530

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HomeHero from '@/components/HomeHero/HomeHero';
 import AboutSection from '@/components/AboutSection/AboutSection';
+import SkillsSection from '@/components/SkillsSection/SkillsSection';
 
 export default function Home() {
     return (
         <>
             <HomeHero />
             <AboutSection />
+            <SkillsSection />
         </>
     );
 }
--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -1,43 +1,14 @@
 import styles from './AboutSection.module.scss';
 import Heading from '../ui/Heading';
-import Subheading from '../ui/Subheading';
 
 function AboutSection() {
     return (
         <div className={styles.about_section}>
+            <Heading>About Me</Heading>
             <div className={styles.about_container}>
-                <Heading>About Me</Heading>
                 <p className={styles.about_content}>
                     I&apos;m a developer who&apos;s really wants to learn new things. I&apos;ve got experience building backend applications and cloud systems. I can picking up new skills in no time and always want to be part of cool, innovative projects.
                 </p>
-            </div>
-
-            <div className={styles.skills_container}>
-                <div className={styles.skills_wrapper}>
-                    <div className={styles.skills_column}>
-                        <div className={styles.skills_category}>
-                            <Subheading>Languages</Subheading>
-                            <p className={styles.skills_list}>Python, Go</p>
-                        </div>
-                        
-                        <div className={styles.skills_category}>
-                            <Subheading>BackEnd</Subheading>
-                            <p className={styles.skills_list}>Django, Django Rest, Golang</p>
-                        </div>
-                    </div>
-                    
-                    <div className={styles.skills_column}>
-                        <div className={styles.skills_category}>
-                            <Subheading>DevOps</Subheading>
-                            <p className={styles.skills_list}>Infrastructure as a Code, Terraform, GitLab - CI, AWS, Kubernetes</p>
-                        </div>
-                        
-                        <div className={styles.skills_category}>
-                            <Subheading>Databases</Subheading>
-                            <p className={styles.skills_list}>MySQL, Postgresql, Dynamodb</p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     );
